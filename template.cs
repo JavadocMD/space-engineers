a2a1f201b848a189fd09d6eb59b580ae73733e7e
Tyler Coles <tylercoles@gmail.com>
2020-01-09 21:43:11 -0700

    Project setup.

---
+++ template.cs
@@ -0,0 +1,40 @@
+#region Prelude
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+// Change this namespace for each script you create.
+namespace SpaceEngineers.TEMPLATE {
+  public sealed class Program : MyGridProgram {
+    // Your code goes between the next #endregion and #region
+    #endregion
+
+    public Program() {
+      // Initialize program.
+    }
+
+    public void Save() {
+      // Save program state.
+    }
+
+    public void Main(string argument, UpdateType updateSource) {
+      // Main logic.
+    }
+
+    #region PreludeFooter
+  }
+}
+#endregion
